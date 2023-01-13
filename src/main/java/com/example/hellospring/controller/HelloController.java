857700c5bad878c7d64afa0d07d8f358930bf530
freemjstudio <mjwoo0@naver.com>
2023-01-13 22:55:12 +0900

    :polar_bear: 01112 static page build

---
+++ src/main/java/com/example/hellospring/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("data","minjee!!");
+        return "hello";
+    }
+}
